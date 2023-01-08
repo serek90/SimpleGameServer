e9b4331f5406429a443bddd72259d9bd5dcc33cc
Jakub Seroczynski <43785920+serek90@users.noreply.github.com>
2023-01-08 14:26:43 +0100

    Make server to use IPv4 and IPv6 and multiclient

--- GameServer.c
+++ GameServer.c
@@ -15,10 +15,77 @@
 #include <netdb.h>
 #include <sys/types.h>
 #include <string.h>
+#include <pthread.h>
+#include <stdatomic.h>
 
 #define MAX_NUM 5
 #define MIN_NUM 1
+#define NTHREADS 40
 
+struct client
+{
+        int fd; //file descriptor
+        int id;
+};
+
+atomic_int endLottery;
+/*******************************
+*net thread function
+*******************************/
+pthread_t netThread[NTHREADS];
+atomic_int netThreadCtr = 0;
+struct client client[NTHREADS];
+
+void *netThreadFunc(void *arg)
+{
+        struct client *client = (struct client*)arg;
+
+        //6. send() recv()
+        const int buffer_size = 1024;
+        char buffer[buffer_size];
+        int ret = recv(client->fd, buffer, buffer_size, 0);
+        if(ret > 0)
+        {
+                printf("Received %d bythes from client % d\n", ret, client->id);
+                for(int i = 0; i < ret; i++)
+                        putchar(buffer[i]);
+        }
+
+
+        while(endLottery){}
+
+        printf("Hey I finished my work\n");
+        /* Close client socket */
+        close(client->fd);
+}
+/******************************
+*Rounding thread
+******************************/
+pthread_t lotteryThread;
+int winning_num;
+
+void *lotteryThreadFunc(void *arg)
+{
+        while(1)
+        {
+                /* if there is a one connected netThear start lottery */
+                if(netThreadCtr)
+                {
+                        printf("DEBUG: Start lottery\n");
+                        endLottery = 1;
+                        sleep(15);
+                        winning_num  = rand()%MAX_NUM + MIN_NUM;
+                        printf("%d\n", winning_num);
+                        endLottery = 0;
+                        netThreadCtr = 0;
+                }
+        }
+}
+
+/*****************************
+* main thread
+* Accepted incoming connections
+*****************************/
 int main()
 {
         srand(time(NULL));
@@ -70,6 +137,9 @@ int main()
         }
         printf("Binding succes\n");
 
+        /* Start lottery thread */
+        pthread_create(&lotteryThread, NULL, lotteryThreadFunc, NULL);
+
         //4. listen()
         /* Listen for incoming connections */
         printf("Listening...\n");
@@ -80,47 +150,32 @@ int main()
         }
 
         //5. accept()
-        printf("Accepting a new connection\n");
         struct sockaddr client_addr;
         socklen_t client_len = sizeof(client_addr);
-        int client_fd = accept(sockfd, &client_addr, &client_len);
-        if(client_fd == -1)
-        {
-                perror("Failed with accepted\n");
-                exit(1);
-        }
-        printf("Connection accepted\n");
-
-        //6. send() recv()
-        const int buffer_size = 1024;
-        char buffer[buffer_size];
-        int ret = recv(client_fd, buffer, buffer_size, 0);
-        if(ret > 0)
-        {
-                printf("Received %d bythes:\n", ret);
-                for(int i = 0; i < ret; i++)
-                        putchar(buffer[i]);
-        }
-        sleep(5);
 
         /* main loop */
         while(1)
         {
-                uint16_t number = rand()%MAX_NUM + MIN_NUM;
-                printf("%d\n", number);
-                int ret = recv(client_fd, buffer, buffer_size, 0);
-                if(ret > 0)
+                //5. accept()
+                printf("Accepting a new connection\n");
+
+                client[netThreadCtr].fd = accept(sockfd, &client_addr, &client_len);
+                if(client[netThreadCtr].fd == -1)
                 {
-                        printf("Received %d bythes:\n", ret);
-                        for(int i = 0; i < ret; i++)
-                                putchar(buffer[i]);
+                        perror("Failed with accepted\n");
+                        exit(1);
                 }
-                sleep(5);
+                printf("Connection accepted\n");
+                client[netThreadCtr].id = netThreadCtr;
+                int result = pthread_create(&netThread[netThreadCtr], NULL, netThreadFunc, (void *) &client[netThreadCtr]);
+                if (result != 0) {
+                        perror("Could not create thread.");
+                }
+                netThreadCtr++;
+
         }
 
         // 7. close()
-        /* Close client socket */
-        close(client_fd);
         /*free allocated memory */
         freeaddrinfo(server_addr);
         /* Clear resources */
