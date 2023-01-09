2cee55d98ae461e8e4f8d7a27f67e675f65dfd4f
Jakub Seroczynski <43785920+serek90@users.noreply.github.com>
2023-01-09 14:29:25 +0100

    Ready game client

--- GameClient.c
+++ GameClient.c
@@ -15,7 +15,7 @@
 #include <netdb.h>
 #include <sys/types.h>
 #include <string.h>
-
+#include "betGameProtocol.h"
 
 int main()
 {
@@ -56,20 +56,63 @@ int main()
         }
         printf("Succesfully connected to the server\n");
 
-        //4.send() recv() in while loop
-        //const int buffer_size = 1024;
-        while(1)
+        //4.send() recv()
+        struct BEGASEP_header *mess_header = calloc(1, sizeof(struct BEGASEP_header));
+        mess_header->Ver = 1;
+        mess_header->Len = sizeof( struct BEGASEP_header);
+        mess_header->Type = BEGASEP_OPEN;
+        mess_header->Client_id = 0;
+        int ret = send(sockfd, mess_header, sizeof(struct BEGASEP_header), 0);
+        if(ret < 1)
         {
-                char buffer[] = "Hello server\n";
-                int ret = send(sockfd, buffer, strlen(buffer), 0);
-                if(ret < 1)
-                {
-                        perror("Send failed\n");
-                        exit(1);
-                }
-                sleep(20);
+                perror("Send failed\n");
+                exit(1);
         }
-        //5.close()
+
+        struct BEGASEP_accept *mess_accept = calloc(1, sizeof(struct BEGASEP_accept));
+        ret = recv(sockfd, mess_accept, sizeof(struct BEGASEP_accept), 0);
+        if(ret > 0)
+        {
+                printf("Received %d bythes \n", ret);
+                printf("ver: %d, len: %d, type: %d, client_id: %d\n", mess_accept->header.Ver,
+                                                                  mess_accept->header.Len,
+                                                                  mess_accept->header.Type,
+                                                                  mess_accept->header.Client_id);
+                printf("Max num: %d, min num: %d\n", mess_accept->lowNumRange, mess_accept->upNumRange);
+
+        }
+
+        /* (3) Receive message BEGASEP_BET */
+        struct BEGASEP_betting *mess_bet = calloc(1, sizeof(struct BEGASEP_betting));
+        mess_bet->header.Ver = 1;
+        mess_bet->header.Len = sizeof( struct BEGASEP_betting);
+        mess_bet->header.Type = BEGASEP_BET;
+        mess_bet->header.Client_id = mess_accept->header.Client_id;
+        mess_bet->bettingNum = 20; //JSJS hardcode
+
+        ret = send(sockfd, mess_bet, sizeof(struct BEGASEP_betting), 0);
+        if(ret < 1)
+        {
+                perror("Send failed\n");
+                exit(1);
+        }
+
+        /* (4) Send message BEGASEP_RESULT */
+        struct BEGASEP_result *mess_result = calloc(1, sizeof(struct BEGASEP_result));
+        ret = recv(sockfd, mess_result, sizeof(struct BEGASEP_result), 0);
+        if(ret > 0)
+        {
+                printf("Received %d bythes\n", ret);
+                printf("ver: %d, len: %d, type: %d, client_id: %d\n", mess_result->header.Ver,
+                                                                  mess_result->header.Len,
+                                                                  mess_result->header.Type,
+                                                                  mess_result->header.Client_id);
+                printf("The great number is: %d and you: %s!!!", mess_result->winNum, mess_result->status ? "won" : "lose");
+
+        }
+
+        /* 5. close socket */
+        close(sockfd);
 
         return 0;
 }
